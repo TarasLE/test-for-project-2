c1c70df2e9c088d815d0b8d6494905178f5208d3
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-12-12 16:43:06 +0200

    start

---
+++ src/js/components/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+    searchForm: document.querySelector('.search-form'),
+    imageContainer: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+    
+}
